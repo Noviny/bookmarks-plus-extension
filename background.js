51b93863906355b9b2b6b6749673b45019b72c07
Noviny BC <benjaminconolly@gmail.com>
2015-12-24 22:49:38 +1100

    Proof of concept app

---
+++ background.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $('#click-me').click( function () {
+    event.preventDefault()
+    searchFor = $('#search').val();
+    chrome.bookmarks.search(searchFor, function (folder) {
+      if ( folder ) {
+        var folderID = folder[0].id;
+        chrome.bookmarks.getChildren('453', function(result) {
+          for (var i = 0; i < result.length; i++) {
+            if ( result[i].url ) {
+              console.log(result[i].title);
+            } else {
+              console.log("THIS WAS A FOLDER");
+            }
+          };
+        });
+      } else {
+        window.alert("No such Folder");
+      };
+    });
+  });
+});
+
+
