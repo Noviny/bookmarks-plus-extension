da722ffc62e75cd9e2351e0a9c92a4d194627ab6
Noviny BC <benjaminconolly@gmail.com>
2016-01-07 00:21:29 +1100

    Can send post requests, need to mod rails app to accept them

--- background.js
+++ background.js
@@ -2,23 +2,24 @@ document.addEventListener('DOMContentLoaded', function () {
   $('#click-me').click( function () {
     event.preventDefault()
     searchFor = $('#search').val();
-    chrome.bookmarks.search(searchFor, function (folder) {
-      if ( folder ) {
-        var folderID = folder[0].id;
-        chrome.bookmarks.getChildren('453', function(result) {
-          for (var i = 0; i < result.length; i++) {
-            if ( result[i].url ) {
-              console.log(result[i].title);
-            } else {
-              console.log("THIS WAS A FOLDER");
-            }
-          };
-        });
-      } else {
-        window.alert("No such Folder");
-      };
-    });
+
+    // $.post('https://journal-maker-app.herokuapp.com/', searchFor)
+
+    // chrome.bookmarks.search(searchFor, function (folder) {
+    //   if ( folder ) {
+    //     var folderID = folder[0].id;
+    //     chrome.bookmarks.getChildren('453', function(result) {
+    //       for (var i = 0; i < result.length; i++) {
+    //         if ( result[i].url ) {
+    //           console.log(result[i].title);
+    //         } else {
+    //           console.log("THIS WAS A FOLDER");
+    //         }
+    //       };
+    //     });
+    //   } else {
+    //     window.alert("No such Folder");
+    //   };
+    // });
   });
 });
-
-
